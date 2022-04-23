32a61e1de7722486e2633106c06136dbe133b0fa
UladzimirKulakou <rombik9@ya.ru>
2022-04-23 17:14:31 +0300

    commit 2
    Main Tab Bar created

---
+++ MusicPlayer/MusicPlayer/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  MusicPlayer
+//
+//  Created by Владимир  on 23.04.22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 0.9617310166, green: 0.2459604144, blue: 0.3703291714, alpha: 1)
+        
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "Search - Selected"), title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "Library - Selected"), title: "Library")
+            
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController{
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+    
+}
--- MusicPlayer/MusicPlayer/SearchViewController.swift
+++ MusicPlayer/MusicPlayer/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MusicPlayer
 //
 //  Created by Владимир  on 23.04.22.
@@ -7,18 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         
         view.backgroundColor = .white
-        
-        
     }
-
-
 }
-
