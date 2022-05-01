cde11a0378eeb00a2298d023354f414d848cb527
UladzimirKulakou <rombik9@ya.ru>
2022-05-01 16:46:16 +0300

    commit

--- MusicPlayer/MusicPlayer/SearchViewController.swift
+++ MusicPlayer/MusicPlayer/SearchViewController.swift
@@ -10,6 +10,8 @@ import Alamofire
 
 
 class SearchViewController: UITableViewController {
+    
+    var networkService = NetworkService()
 
     private var timer: Timer?
 
@@ -55,32 +57,12 @@ extension SearchViewController: UISearchBarDelegate {
         print(searchText)
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { (_) in
-            let url = "https://itunes.apple.com/search"
-            let parameters = ["term":"\(searchText)",
-                             "limit":"10"]
-            AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
-                if let error = dataResponse.error {
-                    print("Error recived requestion data: \(error.localizedDescription)")
-                    return
-                }
-
-                guard let data = dataResponse.data else { return }
-                
-                let decoder = JSONDecoder()
-                do {
-                    let objects = try decoder.decode(SearchResponse.self, from: data)
-                    print("objects:", objects)
-                    self.tracks = objects.results
-                    self.tableView.reloadData()
-                    
-                } catch let jsonError {
-                    print ("Faild to decode JSON", jsonError)
-                }
-                
-//                let someString = String(data: data, encoding: .utf8)
-//                print(someString ?? "")
-                
+            self.networkService.fetchTracks(searchText: searchText) { [weak self] (searchResults) in
+                self?.tracks = searchResults?.results ?? []
+                self?.tableView.reloadData()
             }
+            
+            
         })
        
     }
