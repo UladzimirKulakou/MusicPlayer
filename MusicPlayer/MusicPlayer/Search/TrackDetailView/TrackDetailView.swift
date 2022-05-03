b8a38b0c0efce280fccd4f7a163e79b7e4b55fae
UladzimirKulakou <rombik9@ya.ru>
2022-05-03 13:10:50 +0300

    commit 8
    create track detail controller

---
+++ MusicPlayer/MusicPlayer/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  MusicPlayer
+//
+//  Created by Владимир  on 3.05.22.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
